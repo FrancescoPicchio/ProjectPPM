9be318dc4567772bc6ba36be102f778810f0f00a
FrancescoPicchio <francesco.picchioni@edu.unifi.it>
2024-05-09 10:35:32 +0200

    added a lot of features and images

--- scripts/headerShrink.js
+++ scripts/headerShrink.js
@@ -1,11 +1,15 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 
 window.onscroll = function() {scrollFunction()};
-
+//FIXME don't change the font size, make it change only the header height
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector("header").style.fontSize = "30px";
+    document.querySelector("#subscribe-box>h3").style.display = "none";
+    document.querySelector("#subscribe-box>p").style.display = "none";
+    document.querySelector("header").style.maxHeight = "80px";
   } else {
-    document.querySelector("header").style.fontSize = "90px";
+    document.querySelector("#subscribe-box>h3").style.display = "block";
+    document.querySelector("#subscribe-box>p").style.display = "block";
+    document.querySelector("header").style.maxHeight = "160px";
   }
 }
