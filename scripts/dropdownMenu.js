9281c4f63016f1945aceda8c837cd76901178f9a
FrancescoPicchio <francesco.picchioni@edu.unifi.it>
2024-05-13 15:37:05 +0200

    added more features to the menu

--- scripts/dropdownMenu.js
+++ scripts/dropdownMenu.js
@@ -3,12 +3,16 @@ const closeMenuButton = document.getElementById("close-menu-icon")
 
 openMenuButton.addEventListener("click", function() {
     console.log("opening button was clicked!");
+    document.querySelector("body").style.overflow = "hidden";
     document.querySelector("#main-column").style.display = "none";
+    document.querySelector("footer").style.display = "none";
     document.querySelector("#menu-panel").style.display = "block";
 });
 
 closeMenuButton.addEventListener("click", function() {
     console.log("closing button was clicked!");
+    document.querySelector("body").style.overflow = "auto";
     document.querySelector("#main-column").style.display = "block";
+    document.querySelector("footer").style.display = "flex";
     document.querySelector("#menu-panel").style.display = "none";
 });
