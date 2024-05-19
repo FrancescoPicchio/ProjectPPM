02ad2f71d721481216af37ded32784416a3e3c91
FrancescoPicchio <francesco.picchioni@edu.unifi.it>
2024-05-19 13:08:33 +0200

    Added responsiveness for the main page

--- scripts/headerShrink.js
+++ scripts/headerShrink.js
@@ -1,16 +1,19 @@
-// When the user scrolls down 50px from the top of the document, resize the header's font size
+//checks if the window size is less than 750px, to avoid it conflicting with the corresponding media queary for that size
 
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector("header").style.maxHeight = "80px";
-    //adjusting the subscribe-box div
-    document.querySelector("#subscribe-box>h3").style.display = "none";
-    document.querySelector("#subscribe-box>p").style.display = "none";
-  } else {
-    document.querySelector("header").style.maxHeight = "100px";
-    //adjusting the subscribe-box div
-    document.querySelector("#subscribe-box>h3").style.display = "block";
-    document.querySelector("#subscribe-box>p").style.display = "block";
+if(window.innerWidth > 750){
+  // When the user scrolls down 50px from the top of the document, resize the header's font size
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.querySelector("header").style.maxHeight = "80px";
+      //adjusting the subscribe-box div
+      document.querySelector("#subscribe-box>h3").style.display = "none";
+      document.querySelector("#subscribe-box>p").style.display = "none";
+    } else {
+      document.querySelector("header").style.maxHeight = "100px";
+      //adjusting the subscribe-box div
+      document.querySelector("#subscribe-box>h3").style.display = "block";
+      document.querySelector("#subscribe-box>p").style.display = "block";
+    }
   }
 }
